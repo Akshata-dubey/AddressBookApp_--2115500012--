5d939d3e183ad11b334d54a2337bcaa8c1487091
Akshata Dubey <akshatadubey213@gmail.com>
2025-03-14 12:45:07 +0530

    adding files

---
+++ RepositoryLayer/Service/AddressBookDBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+using ModelLayer.DTO;
+
+namespace RepositoryLayer.Service
+{
+    public class AddressBookDBContext : DbContext
+    {
+        public AddressBookDBContext(DbContextOptions<AddressBookDBContext> options)
+            : base(options) { }
+
+        public DbSet<UserEntity> Users { get; set; }
+        public DbSet<AddressBookEntityEntry> AddressBookEntries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserEntity>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+        }
+    }
+}
